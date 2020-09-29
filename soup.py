e3290779f1a0fc932596bc8cc4af78bfa580c1e3
rahu96 <rahulcruise96@gmail.com>
2020-09-30 00:42:24 +0530

    added option to search job

--- soup.py
+++ soup.py
@@ -2,21 +2,23 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib3
+import re
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://www.technopark.org/job-search'
+job = input('Job Title: ')
 r = requests.get(url,verify=False)
 soup = BeautifulSoup(r.content,'html.parser')
 for l in soup.find_all('tr', {'class' : 'companyList'}):
 
-	
+	if re.search(job, l.a.text,  re.IGNORECASE):
 
-	jobTitle = l.a.text
-	jobLink = l.a['href']
-	company = l.select('a')[1].text
-	companyLink = l.select('a')[1]['href']
-	
-	print(f"Job: {jobTitle} , link: technopark.org/{jobLink} ")
-	print(f"Company: {company}, Link: technopark.org{companyLink} ")
-	
-	
-	print()
+		jobTitle = l.a.text
+		jobLink = l.a['href']
+		company = l.select('a')[1].text
+		companyLink = l.select('a')[1]['href']
+		
+		print(f"Job: {jobTitle} , link: technopark.org/{jobLink} ")
+		print(f"Company: {company}, Link: technopark.org{companyLink} ")
+		
+		
+		print()
